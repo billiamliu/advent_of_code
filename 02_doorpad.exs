e0971d8d15d10922be7568dd095587e47643e488
Billiam <hello@billiam.io>
2016-12-03 21:49:53 -0800

    solved 2

--- 02_doorpad.exs
+++ 02_doorpad.exs
@@ -34,3 +34,61 @@ defmodule One do
 end
 
 input |> One.run |> IO.inspect
+
+defmodule Two do
+
+  defp grid do
+    [
+      [ nil, nil, 1, nil, nil ],
+      [ nil, 2,   3,   4,   nil ],
+      [ 5,   6,   7,   8,   9 ],
+      [ nil, "A", "B", "C", nil ],
+      [ nil, nil, "D", nil, nil ]
+    ]
+  end
+
+  def run( input ) do
+    input
+    |> Enum.scan( { 2, 2 }, &step/2 )
+  end
+
+  defp step( [], res ), do: res
+
+  defp step( [ "U" | t ], { y, x } ) do
+    if y > 0 and grid |> Enum.at( y - 1 ) |> Enum.at( x ) !== nil do
+      step( t, { y - 1, x } )
+    else
+      step( t, { y, x } )
+    end
+  end
+
+  defp step( [ "D" | t ], { y, x } ) do
+    if y < 4 and grid |> Enum.at( y + 1 ) |> Enum.at( x ) !== nil do
+      step( t, { y + 1, x } )
+    else
+      step( t, { y, x } )
+    end
+  end
+
+  defp step( [ "L" | t ], { y, x } ) do
+    if x > 0 and grid |> Enum.at( y ) |> Enum.at( x - 1 ) !== nil do
+      step( t, { y, x - 1 } )
+    else
+      step( t, { y, x } )
+    end
+  end
+
+  defp step( [ "R" | t ], { y, x } ) do
+    if x < 4 and grid |> Enum.at( y ) |> Enum.at( x + 1 ) !== nil do
+      step( t, { y, x + 1 } )
+    else
+      step( t, { y, x } )
+    end
+  end
+
+end
+
+# "ULLL\nDRU\nDDDDDDDDDDDDRRRRRURUU" |> String.split( "\n" ) |> Enum.map( &String.graphemes/1 )
+input
+|> Two.run
+|> IO.inspect
